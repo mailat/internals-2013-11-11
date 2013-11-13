22b49ba1e63054cbcb1dc75ad8511f8ee2b22d6e
Marius Mailat <marius.mailat@appseleration.com>
2013-11-13 14:35:59 +0400

    + customized x86 kernel, a copy of goldfish

--- Platform build/marakana/alpha/common.mk
+++ Platform build/marakana/alpha/common.mk
@@ -14,6 +14,15 @@ PRODUCT_COPY_FILES += system/core/rootdir/etc/vold.fstab:system/etc/vold.fstab
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml
     
-    
 # Enable overlays
 DEVICE_PACKAGE_OVERLAYS := $(MY_PATH)/overlay
+
+# Enable our custom kernel
+LOCAL_KERNEL := $(MY_PATH)/kernel
+PRODUCT_COPY_FILES += $(LOCAL_KERNEL):kernel
+
+# Copy our init and ueventd configuration files to the root
+# file system (ramdisk.img -> boot.img)
+PRODUCT_COPY_FILES += $(MY_PATH)/init.goldfish.rc:root/init.goldfish.rc
+PRODUCT_COPY_FILES += $(MY_PATH)/ueventd.goldfish.rc:root/ueventd.goldfish.rc
+
